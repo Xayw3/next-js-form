0c4cbfee52cf4c61b94e31c2838bb939607b00d3
Edgars Kapilinskis <85979954+Xayw3@users.noreply.github.com>
2022-10-30 20:10:06 +0200

    Update success.tsx

--- pages/success.tsx
+++ pages/success.tsx
@@ -19,7 +19,7 @@ const Success = () => {
 
   return (
     <m.main className='h-screen flex items-center justify-center'>
-      <div className='bg-white rounded-lg w-1/2 text-gray-700 p-16' >
+      <div className='bg-white rounded-lg 2xl:w-1/2 text-gray-700 p-16' >
         <h1 className='text-3xl pb-4 font-bold'>
           Thanks for the Email {router.query.name}!
         </h1>
